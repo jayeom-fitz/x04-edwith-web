9974a0afdf8b851407878fdbf222dee6c7ef2ca3
JayEom <djawjdgus93@naver.com>
2020-12-13 22:46:53 +0900

    #0-2 Project Init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <>
+      <Header />
+      <div>
+        App
+      </div>
+      <Footer />
+    </>
   );
 }
 
