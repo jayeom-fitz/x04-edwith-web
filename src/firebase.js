71433a5d8618817b80fbac0319d55b3a4dc8c986
jayeom-fitz <djawjdgus93@naver.com>
2020-12-19 00:11:38 +0900

    #A-1 Firebase Init

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDDpr02nUE1RtQ-okFNC9MF9m6KJEQfWu0",
+    authDomain: "edwith-8cb44.firebaseapp.com",
+    projectId: "edwith-8cb44",
+    storageBucket: "edwith-8cb44.appspot.com",
+    messagingSenderId: "758785908844",
+    appId: "1:758785908844:web:4f14e56c0e0c07740b3849"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
--- src/project-todo/routes/Todo.js
+++ src/project-todo/routes/Todo.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import styled from "styled-components";
+
+const Container = styled.div`
+  top: 0;  
+  padding-top: 100px;
+`;
 
 function Todo() {
   return (
     <>
       <Header />
-      <div>Project A</div>
+      <Container>
+        Project A
+      </Container>
       <Footer />
     </>
   );
